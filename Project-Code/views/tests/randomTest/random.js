016ed0b7015b5c17bbdcb8e56910b89f2d41a830
Katkhade Dattatraya Jankiram <67061292+knight-furry@users.noreply.github.com>
2022-10-28 16:36:41 +0530

    Add files via upload

---
+++ Project-Code/views/tests/randomTest/random.js
@@ -0,0 +1,243 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which of the following usees FIFO method",
+        answer: "Queue",
+        options: [
+          "Queue",
+          "stack",
+          "linklist",
+          "binary tree"
+        ]
+  },
+  
+
+    {
+        numb: 2,
+    question: "____________form of access is used to add remove nodes from a stack.",
+    answer: "LIFO",
+    options: [
+      "LIFO",
+      "FIFO",
+      "none",
+      "all"
+    ]
+    
+  }, 
+  {
+    numb: 3,
+    question: "For the following question, how will the array elements look like after second pass? 34, 8, 64, 51, 32, 21",
+    answer: "8, 34, 64, 51, 32, 21",
+    options: [
+      "8, 21, 32, 34, 51, 64",
+      "8, 32, 34, 51, 64, 21",
+      "8, 34, 51, 64, 32, 21",
+      "8, 34, 64, 51, 32, 21"
+    ]
+  },
+  {
+    numb: 4,
+    question: "In the following scenarios, when will you use selection sort?",
+    answer: "Large values need to be sorted with small keys",
+    options: [
+      "The input is already sorted",
+      "A large file has to be sorted",
+      "Large values need to be sorted with small keys",
+      "Small values need to be sorted with large keys"
+    ]
+},
+
+  {
+    numb: 5,
+    question: " The given array is arr = {1, 2, 4, 3}. Bubble sort is used to sort the array elements. How many iterations will be done to sort the array?",
+    answer: "4",
+    options: [
+      "4",
+      "2",
+      "1",
+      "0"
+    ]
+  },
+ 
+    {
+    numb: 6,
+    question: "Quick sort follows Divide-and-Conquer strategy.",
+    answer: "True",
+    options: [
+      "True",
+      "False",
+      "-",
+      "-"
+    ]
+  },
+  
+  {
+    numb: 7,
+    question: "What is a node used for in a linked list?",
+    answer: "To store the information and the link to the next item",
+    options: [
+      "To store the information and the link to the next item",
+      "To check for the end of the list",
+      "Not used in a linked list",
+      "none"
+    ]
+  },
+ 
+
+  {
+    numb: 8,
+    question: "What will be the initial value with which top is initialized",
+    answer: "-1",
+    options: [
+      "-1",
+      "garbage",
+      "1",
+      "none of above"
+    ]
+  },  
+  
+  {
+    numb: 9,
+    question: "Which of the following sorting algorithms is used along with quick sort to sort the sub arrays?",
+    answer: "Insertion sort",
+    options: [
+      "Merge sort",
+      "Shell sort",
+      "Insertion sort",
+      "Bubble sort"
+    ]
+  }, 
+  
+
+  {
+    numb: 10,
+    question: "Which of the following is true about merge sort?",
+    answer: "All of the above",
+    options: [
+      "Merge Sort works better than quick sort if data is accessed from slow sequential memory.",
+      "Merge Sort is stable sort by nature",
+      "Merge sort outperforms heap sort in most of the practical situations.",
+      "All of the above"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "What is the worst case complexity of bubble sort?",
+    answer: "O(n2)",
+    options: [
+      "O(nlogn)",
+      "O(logn)",
+      "O(n)",
+      "O(n2)"
+    ]
+  },
+  {
+    numb: 12,
+    question: " Linked list is generally considered as an example of _________ type of memory allocation.",
+    answer: "Dynamic",
+    options: [
+      "Static",
+      "Compile Time",
+      "Dynamic",
+      "none"
+    ]
+  },
+  {
+    numb: 13,
+    question: "In stack if you try to remove element from empty stack, then its is called as ",
+    answer: "stack underflow",
+    options: [
+      "empty collection",
+      "stack underflow",
+      "stack empty",
+      "garbage collection"
+    ]
+  },
+
+
+  {
+    numb: 14,
+    question: "Which of the following is not a stable sorting algorithm?",
+    answer: "Merge sort",
+    options: [
+      "Quick sort",
+      "Cocktail sort",
+      "Bubble sort",
+      "Merge sort"
+    ]
+  },    
+  {
+    numb: 15,
+    question: "What is the best case complexity of selection sort?",
+    answer: "O(n2)",
+    options: [
+      "O(nlogn)",
+      "O(logn)",
+      "O(n)",
+      "O(n2)"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Which of the following is application of stack",
+    answer: "all of these",
+    options: [
+      "tower of hanoi",
+      "finding factorial",
+      "infix to postfix",
+      "all of these"
+    ]
+  }, 
+  {
+    numb: 17,
+    question: "How many passes does an insertion sort algorithm consist of?",
+    answer: "N-1",
+    options: [
+      "N",
+      "N-1",
+      "N+1",
+      "N2"
+    ]
+},
+{
+    numb: 18,
+    question: "In a bubble sort structure, there is/are?",
+    answer: "Two for loops, one nested in the other",
+    options: [
+      "single for loop",
+      "Three for loops, all separate",
+      "while loop",
+      "Two for loops, one nested in the other"
+    ]
+  }, 
+  
+{
+    numb: 19,
+    question: "Which of the following sorting algorithm does not use recursion?",
+    answer: "bottom up merge sort",
+    options: [
+      "quick sort",
+      "merge sort",
+      "heap sort",
+      "bottom up merge sort"
+    ]
+  }, 
+{
+    numb: 20,
+    question: "What is the first change that selection sort would make to this sequence to put it into ascending order: 3 1 4 2",
+    answer: "1 3 4 2",
+    options: [
+      "1 3 4 2",
+      "2 4 1 3",
+      "3 2 4 1",
+      "4 2 3 1"
+    ]
+  }, 
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+ 
+  
+];
---
+++ Project-Code/views/tests/tests/Stack/que_test.js
@@ -0,0 +1,120 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Pushing an element into stack already having five elements and stack size of 5, then stack becomes ___________",
+    answer: "Overflow",
+    options: [
+      "Overflow",
+      "Crash",
+      "Underflow",
+      "User flow"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is not the application of stack?",
+    answer: "Data Transfer between two asynchronous process",
+    options: [
+      "A parentheses balancing program",
+      "Tracking of local variables at run time",
+      "Compiler Syntax Analyzer",
+      "Data Transfer between two asynchronous process"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the value of the postfix expression 6 3 2 4 + – *?",
+    answer: "-18",
+    options: [
+      "1",
+      "40",
+      "74",
+      "-18"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The postfix form of A*B+C/D is?",
+    answer: "AB*CD/+",
+    options: [
+      "*AB/CD+",
+      "AB*CD/+",
+      "A*BC+/D",
+      "ABCD+/*"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The type of expression in which operator succeeds its operands is?",
+    answer: "Postfix Expression",
+    options: [
+      "Infix Expression",
+      "Prefix Expression",
+      "Postfix Expression",
+      "Both Prefix and Postfix Expressions"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "Process of inserting an element in stack is called ____________",
+    answer: "Push",
+    options: [
+      "Create",
+      "Push",
+      "Evaluation",
+      "Pop"
+    ]
+  },
+
+    {
+    numb: 7,
+    question: "Process of removing an element from stack is called __________",
+    answer: "Pop",
+    options: [
+      "Create",
+      "Push",
+      "Evaluation",
+      "Pop"
+    ]
+  },
+
+    {
+    numb: 8,
+    question: "The data structure required to check whether an expression contains a balanced parenthesis is?",
+    answer: "Stack",
+    options: [
+      "Stack",
+      "Queue",
+      "Array",
+      "Tree"
+    ]
+  },
+
+    {
+    numb: 9,
+    question: "The prefix form of an infix expression (p + q) – (r * t) is?",
+    answer: "– +pq * rt",
+    options: [
+      "+ pq – *rt",
+      "– +pqr * t",
+      "– +pq * rt",
+      "– + * pqrt"
+    ]
+  },
+
+    {
+    numb: 10,
+    question: "Which data structure is used for implementing recursion?",
+    answer: "Stack",
+    options: [
+      "Queue",
+      "Stack",
+      "Array",
+      "List"
+    ]
+  },
+];
